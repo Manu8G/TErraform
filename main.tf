56a1988b38352c1ae787d99352e05f1943ab8626
Manuel Guerrero Mesias <manuel8guerrero@hotmail.com>
2024-12-17 19:00:38 +0100

    duplicado de servidor correctamente

--- main.tf
+++ main.tf
@@ -1,17 +1,32 @@
+
+# Definimos el provider como AWS
 provider "aws" {
   region = "eu-west-1"
   shared_config_files = [ "/home/manuel/.aws/config" ]
   shared_credentials_files = [ "/home/manuel/.aws/credentials" ]
 }
 
-resource "aws_instance" "mi_servidor" {
+
+# 
+data "aws_subnet" "aviability_zone_a"{
+    availability_zone = "eu-west-1a"
+}
+
+#
+data "aws_subnet" "aviability_zone_b"{
+    availability_zone = "eu-west-1b"
+}
+
+# Creamos la instancia 1 de EC2 con AMI Ubuntu 22
+resource "aws_instance" "Servidor_1" {
   ami = "ami-0a422d70f727fe93e"
   instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.aviability_zone_a.id
   vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hola terraform!" > index.html
+              echo "Hola terraform! Aqui servidor 1" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 
@@ -20,6 +35,26 @@ resource "aws_instance" "mi_servidor" {
   }
 }
 
+# Creamos la instancia 1 de EC2 con AMI Ubuntu 22
+resource "aws_instance" "Servidor_2" {
+  ami = "ami-0a422d70f727fe93e"
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.aviability_zone_b.id
+  vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hola terraform! Aqui servidor 2" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "servidor-2"
+  }
+}
+
+
+#
 resource "aws_security_group" "mi_grupo_de_seguridad" {
   name = "primer-servidor-sg"
 
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
-output "dns_publica" {
+output "dns_publica_server_1" {
   description = "DNS publica del servidor"
-  value = "http://${aws_instance.mi_servidor.public_dns}:8080"
+  value = "http://${aws_instance.Servidor_1.public_dns}:8080"
 }
 
-output "IPV4" {
+output "IPV4_server_1" {
   description = "IPV4 publica del servidor"
-  value = "http://${aws_instance.mi_servidor.public_ip}:8080"
+  value = "http://${aws_instance.Servidor_1.public_ip}:8080"
+}
+
+output "dns_publica_server_2" {
+  description = "DNS publica del servidor"
+  value = "http://${aws_instance.Servidor_2.public_dns}:8080"
+}
+
+output "IPV4_server_2" {
+  description = "IPV4 publica del servidor"
+  value = "http://${aws_instance.Servidor_2.public_ip}:8080"
 }
